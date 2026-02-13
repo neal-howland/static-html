b4a09261a81bfc54024ade099d3261165bdb1a5e
nealhowland <nhowland@hme.com>
2026-02-13 12:54:16 -0800

    ch2 l1 initial attempt

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextType, TextNode
+
+
+def main():
+    node = TextNode("This is some anchor text", TextType.LINK, "https://boot.dev")
+    print(node)
+
+
+if __name__ == "__main__":
+    main()
